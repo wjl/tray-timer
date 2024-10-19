f05d705285f0363a61b29738a47cf3fda21e5565
Wesley J. Landaker <wjl@icecavern.net>
2024-10-18 22:06:47 -0600

    Assume we're running the build script already in a venv.

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e -u
 
-# Create and activate our venv.
-pyenv exec python -m venv venv
-source ./venv/bin/activate
-
 # Install dependencies.
 pip install --upgrade pip
 pip install \
