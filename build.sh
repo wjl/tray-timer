400793da7fd375827765791b9abccaff74c73b3d
Wesley J. Landaker <wjl@icecavern.net>
2024-10-18 22:02:26 -0600

    Don't use pyenv in the build script, assume we have Python already.
    This makes the script more flexible to be run against any version of
    Python, whether it comes from a package, pyenv, a docker install, etc.

--- build.sh
+++ build.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -e -u
 
-python_version=3.12.7
-
-# Check for pyenv.
-if ! command -v pyenv >& /dev/null; then
-	echo "pyenv not installed."
-	exit 1
-fi
-
-# Install our desired Python version.
-pyenv install --skip-existing "$python_version"
-pyenv local "$python_version"
-
 # Create and activate our venv.
 pyenv exec python -m venv venv
 source ./venv/bin/activate
